e6ed472fb9ea7a31ec5147ca0be0fea9da3fbf64
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-03-31 13:46:47 +0300

    updated formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,102 @@
-const {src, dest} = require("gulp");
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-cssbeautify");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass")(require('sass'));
-const cssnano = require("gulp-cssnano");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const rigger = require("gulp-rigger");
-const concat = require("gulp-concat");
+const { src, dest } = require('gulp');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const rigger = require('gulp-rigger');
+const concat = require('gulp-concat');
 const webp = require('gulp-webp');
-const browserSync = require("browser-sync").create();
+const browserSync = require('browser-sync').create();
 
 function serve() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
 }
 
 function html() {
-    return src('src/**/*.html')
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({stream: true}))
+  return src('src/**/*.html')
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
-    return src(['src/sass/*.scss', 'src/blocks/**/*.scss'])
-        .pipe(concat("style.css"))    
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(cssbeautify())
-        .pipe(cssnano({
-            zindex: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(dest('dist/css/'))
-        .pipe(browserSync.reload({stream: true}));
+  return src(['src/sass/*.scss', 'src/blocks/**/*.scss'])
+    .pipe(concat('style.css'))
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssbeautify())
+    .pipe(cssnano({
+      zindex: false,
+      discardComments: {
+        removeAll: true,
+      },
+    }))
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.css',
+    }))
+    .pipe(dest('dist/css/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function js() {
-    return src('src/**/*.js')
-        .pipe(concat("index.js")) 
-        .pipe(plumber())
-        .pipe(rigger())
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
-        .pipe(dest('dist/js/'))
-        .pipe(browserSync.reload({stream: true}));
+  return src('src/**/*.js')
+    .pipe(concat('index.js'))
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.js',
+    }))
+    .pipe(dest('dist/js/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function images() {
-    return src('src/blocks/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}', 'src/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}')
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 80, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
-        .pipe(webp())
-        .pipe(rename({dirname: ''}))
-        .pipe(dest('dist/images'))
-        .pipe(browserSync.reload({stream: true}));
+  return src('src/blocks/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}', 'src/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}')
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 80, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false },
+        ],
+      }),
+    ]))
+    .pipe(webp())
+    .pipe(rename({ dirname: '' }))
+    .pipe(dest('dist/images'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function video() {
-    return src('src/**/*.mp4')
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({stream: true}))
+  return src('src/**/*.mp4')
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function clean() {
-    return del('./dist');
+  return del('./dist');
 }
 
 function watchFiles() {
-    gulp.watch(['src/**/*.html'], html);
-    gulp.watch(['src/**/*.scss'], css);
-    gulp.watch(['src/**/*.js'], js);
-    gulp.watch(['src/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}'], images);
-    gulp.watch(['src/video/*.mp4'], video)
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/**/*.scss'], css);
+  gulp.watch(['src/**/*.js'], js);
+  gulp.watch(['src/**/*.{jpg,jpeg,png,svg,gif,ico,webp,xml}'], images);
+  gulp.watch(['src/video/*.mp4'], video);
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, js, images, video));
